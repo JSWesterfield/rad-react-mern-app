36fa830e47a4bb6aeddfcb4e1aede0a6b88c94a6
Jake Westerfield <Jacob_Westerfield@discovery.com>
2022-02-02 03:25:56 -0500

    adds in mongodb connection string to lists instead of default random word. lists instead of animals

--- list-backend/index.js
+++ list-backend/index.js
@@ -4,16 +4,11 @@ const app = express();
 
 const MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('mongodb://localhost:27017/animals', function (err, client) {
+MongoClient.connect('mongodb://localhost:27017/lists', function (err, client) {
     if (err) throw err
 
-    var db = client.db('animals')
+    var db = client.db('lists')
 
-    db.collection('mammals').find().toArray(function (err, result) {
-        if (err) throw err
-
-        console.log(result)
-    })
 })
 
 app.get('/', (req, res) => {
