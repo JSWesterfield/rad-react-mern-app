116942c4925d647dbd7e3bbeb03d2560ed481369
Jake Westerfield <Jacob_Westerfield@discovery.com>
2022-02-02 02:42:13 -0500

    adds in MongoClient connecting to localhost example, and creates a db var for mongoCliebt to connect to random database example

--- list-backend/index.js
+++ list-backend/index.js
@@ -2,6 +2,20 @@ const express = require('express');
 
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://localhost:27017/animals', function (err, client) {
+    if (err) throw err
+
+    var db = client.db('animals')
+
+    db.collection('mammals').find().toArray(function (err, result) {
+        if (err) throw err
+
+        console.log(result)
+    })
+})
+
 app.get('/', (req, res) => {
     res.json('did this work');
 })
